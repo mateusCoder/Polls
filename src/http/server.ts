bd28b2e1638c522413b70cec8a7218b3cb3a34cf
mateusCoder <mateus.moraes0507@gmail.com>
2024-02-12 20:38:06 -0300

    feat: adding GET method for find poll by id

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,12 @@ import fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import { string, z } from 'zod'
 import { createPoll } from './routes/create-polls'
+import { getPoll } from './routes/get-polls'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server running!')
