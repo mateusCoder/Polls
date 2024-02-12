08db8fd6005c4a4e84850511bc3821392d1d7b63
mateusCoder <mateus.moraes0507@gmail.com>
2024-02-12 19:16:54 -0300

    feat: adding POST method for poll creation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,11 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { string, z } from 'zod'
+import { createPoll } from './routes/create-polls'
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return "Hello"
-})
+app.register(createPoll)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server running!')
---
+++ src/lib/prisma.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@prisma/client";
+import { log } from "console";
+
+export const prisma = new PrismaClient({log: ['query']})
