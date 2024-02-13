3bf4a9b0201613b2a5408730431ddddf03472bbd
mateusCoder <mateus.moraes0507@gmail.com>
2024-02-13 15:22:19 -0300

    feat: adding POST method for vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,13 +1,21 @@
 import fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import { string, z } from 'zod'
-import { createPoll } from './routes/create-polls'
-import { getPoll } from './routes/get-polls'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie  from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(require('@fastify/cookie'), {
+    secret: "poll-app", 
+    hook: 'onRequest'
+  })
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server running!')
